eac800ee07576868bd87a173b6d4793a236ecfa7
NikitaSalnykov <salnikov.nkt@gmail.com>
2023-06-01 01:14:05 +0300

    update script

--- src/index.js
+++ src/index.js
@@ -7,12 +7,13 @@ const axios = require('axios').default;
 
 const API_KEY = `36868675-d04d7da5b1942ea7b304f9f1a`;
 const BASE_URL = `https://pixabay.com/api/`
+const perPage = 40
 
 const formEl = document.querySelector('.search-form');
 const listEL = document.querySelector('.search-list-js')
 const inputEl = document.querySelector('input[name="searchQuery"]')
-
-
+const target = document.querySelector('.js-guard');
+let gallerySlider;
 
 function onClickSubmit(event) {
 
@@ -23,9 +24,9 @@ function onClickSubmit(event) {
 
 }
 
-function getImages(page) {
+function getImages(page, perPage) {
     
-    getFetch(page)
+    getFetch(page, perPage)
         .then((response) => {
             if (response.data.hits.length === 0) {
                 Notiflix.Notify.failure(`Oooops! No images found for query <i>'${inputEl.value}</i>'`);
@@ -35,8 +36,9 @@ function getImages(page) {
             Notiflix.Notify.success(`Hooray! We found ${response.data.total} images`);
             console.log(response.data);
             listEL.innerHTML = createMarkup(response.data.hits)
+            observer.observe(target);
             
-            let gallerySlider = new SimpleLightbox('.gallery a',
+            gallerySlider = new SimpleLightbox('.gallery a',
             { captionsData: 'alt', captionDelay: 950, navText: ['❮', '❯'] });
 
         })
@@ -45,8 +47,9 @@ function getImages(page) {
         })
 }
 
-function getFetch(page) {
-    const params = new URLSearchParams({ image_types: 'photo', orientation: 'horizontal', safesearch: 'true', per_page: 40, page, })
+function getFetch(page, perPage) {
+    
+    const params = new URLSearchParams({ image_types: 'photo', orientation: 'horizontal', safesearch: 'true', per_page: perPage, page, })
     const searchResponse = inputEl.value.split(' ').join('+')
     return axios.get(`${BASE_URL}?key=${API_KEY}&q=${searchResponse}&${params}`)
 }
@@ -78,3 +81,35 @@ function createMarkup(arr) {
 
 formEl.addEventListener('submit', onClickSubmit);
 
+let options = {
+  root: null,
+  rootMargin: '200px',
+  threshold: 1.0
+};
+
+let observer = new IntersectionObserver(onLoad, options);
+let currentPage = 1
+
+
+function onLoad(entries, observer) {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      console.log(entries);
+      currentPage += 1
+      getFetch(currentPage, perPage)
+          .then(response => {
+          console.log();
+              listEL.insertAdjacentHTML('beforeend', createMarkup(response.data.hits))
+            gallerySlider.refresh()
+              if (currentPage * perPage >= response.data.totalHits) {
+              setTimeout(() => {
+                Notiflix.Notify.info(`We're sorry, but you've reached the end of search results`);
+              }, 2000);
+          observer.unobserve(target)
+              }
+              
+})
+  .catch(err => console.log(err));
+  }
+  })
+  }
